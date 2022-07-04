e55790011d963f59f6941cd925ac279be95d359f
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2022-07-04 08:31:52 -0400

    Adição de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
+
 export const App = () => {
   return (
-    <div className="App">
-      <h1>Oi</h1>
-      
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export const AppRoutes = () => {
+
+    return (
+        <Routes>
+            <Route path='/' element={<p>Página inicial</p>} />
+            <Route path='*' element={<Navigate to="/" />} />
+        </Routes>
+    )
+}
