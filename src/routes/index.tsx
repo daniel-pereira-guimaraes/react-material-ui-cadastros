4ec010f9435925432c93917d4f57b65796eb1d8b
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2022-07-04 09:27:05 -0400

    Adição de LightTheme

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 export const AppRoutes = () => {
 
     return (
         <Routes>
-            <Route path='/' element={<p>Página inicial</p>} />
+            <Route path='/' element={
+                <Button variant='contained' color='primary'>Teste</Button>
+            } />
             <Route path='*' element={<Navigate to="/" />} />
         </Routes>
     )
