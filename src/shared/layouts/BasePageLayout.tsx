963f377bb4069d4334ccc4e3ced188dcd0939982
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2022-07-05 09:01:27 -0400

    Adição de prop toolBar e ajustes da responsividade em BasePageLayout

--- src/shared/layouts/BasePageLayout.tsx
+++ src/shared/layouts/BasePageLayout.tsx
@@ -6,36 +6,52 @@ import { useDrawerContext } from "../contexts";
 interface IBasePageLayoutProps {
   children: React.ReactNode;
   title: string;  
+  toolBar?: React.ReactNode;
 }
 
-export const BasePageLayout: React.FC<IBasePageLayoutProps> = ({ children, title }) => {
+export const BasePageLayout: React.FC<IBasePageLayoutProps> = ({ children, title, toolBar }) => {
 
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+  const mdDown = useMediaQuery(theme.breakpoints.down('md'));
   const { toggleDrawerOpen } = useDrawerContext();
 
   return (
     <Box height="100%" display="flex" flexDirection="column" gap={1} >
 
-      <Box padding={1} height={theme.spacing(12)} display="flex" alignItems="center" gap={1}>
-
+      <Box 
+        padding={1} 
+        height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)} 
+        display="flex" 
+        alignItems="center" 
+        gap={1}
+      >
+      
         {smDown && 
           <IconButton onClick={toggleDrawerOpen}>
             <Icon>menu</Icon>
           </IconButton>
         }
-
-        <Typography variant="h5" component="h2">
+      
+        <Typography 
+          variant={smDown ? 'h5' : mdDown ? 'h4' : 'h3'}
+          component="h2"
+          whiteSpace="nowrap"
+          overflow="hidden"
+          textOverflow="ellipsis"
+        >
           {title}
         </Typography>
-
+      
       </Box>
 
-      <Box>
-        Barra de ferramentas
-      </Box>
+      {toolBar && 
+        <Box>
+          {toolBar}
+        </Box>
+      }
 
-      <Box>
+      <Box flex={1} overflow="auto">
         {children}
       </Box>
     </Box>
