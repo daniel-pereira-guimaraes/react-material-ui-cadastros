435e22d961d24469f10ced432d04ad0482ceedd1
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2022-07-05 20:53:06 -0400

    Adição de props em EditToolBar

--- src/shared/components/edit-tool-bar/EditToolBar.tsx
+++ src/shared/components/edit-tool-bar/EditToolBar.tsx
@@ -1,7 +1,33 @@
 import React from "react";
 import { Box, Paper, useTheme, Button, Icon, Divider } from '@mui/material';
 
-export const EditToolBar: React.FC = () => {
+interface IEditToolBarProps {
+  newButtonText?: string;
+  saveButtonVisible?: boolean;
+  saveAndBackButtonVisible?: boolean;
+  deleteButtonVisible?: boolean;
+  newButtonVisible?: boolean;
+  backButtonVisible?: boolean;
+  saveButtonOnClick?: () => void;
+  saveAndBackButtonOnClick?: () => void;
+  deleteButtonOnClick?: () => void;
+  newButtonOnClick?: () => void;
+  backButtonOnClick?: () => void;
+}
+
+export const EditToolBar: React.FC<IEditToolBarProps> = ({
+  newButtonText = 'Novo',
+  saveButtonVisible = true,
+  saveAndBackButtonVisible = false,
+  deleteButtonVisible = true,
+  newButtonVisible = true,
+  backButtonVisible = true,
+  saveButtonOnClick,
+  saveAndBackButtonOnClick,
+  deleteButtonOnClick,
+  newButtonOnClick,
+  backButtonOnClick
+}) => {
 
   const theme = useTheme();
 
@@ -14,60 +40,66 @@ export const EditToolBar: React.FC = () => {
       display="flex"
       alignItems="center"
       height={theme.spacing(5)}
-      component={Paper} 
-    >
-          
-      <Button 
-        variant="contained" 
-        color="primary" 
-        disableElevation
-        startIcon={<Icon>save</Icon>}
-        onClick={undefined}
-      >
-        Salvar
-      </Button>
+      component={Paper}>
+         
+      {saveButtonVisible &&
+        <Button 
+          variant="contained" 
+          color="primary" 
+          disableElevation
+          startIcon={<Icon>save</Icon>}
+          onClick={saveButtonOnClick}>
+            Salvar
+        </Button>
+      }
 
-      <Button 
-        variant="outlined" 
-        color="primary" 
-        disableElevation
-        startIcon={<Icon>save</Icon>}
-        onClick={undefined}
-      >
-        Salvar e voltar
-      </Button>
+      {saveAndBackButtonVisible &&
+        <Button 
+          variant="outlined" 
+          color="primary" 
+          disableElevation
+          startIcon={<Icon>save</Icon>}
+          onClick={saveAndBackButtonOnClick}>
+            Salvar e voltar
+        </Button>
+      }
 
-      <Button 
-        variant="outlined" 
-        color="primary" 
-        disableElevation
-        startIcon={<Icon>delete</Icon>}
-        onClick={undefined}
-      >
-        Apagar
-      </Button>
+      {deleteButtonVisible && 
+        <Button 
+          variant="outlined" 
+          color="primary" 
+          disableElevation
+          startIcon={<Icon>delete</Icon>}
+          onClick={deleteButtonOnClick}>
+            Apagar
+        </Button>
+      }
 
-      <Button 
-        variant="outlined" 
-        color="primary" 
-        disableElevation
-        startIcon={<Icon>add</Icon>}
-        onClick={undefined}
-      >
-        Novo
-      </Button>
+      {newButtonVisible && 
+        <Button 
+          variant="outlined" 
+          color="primary" 
+          disableElevation
+          startIcon={<Icon>add</Icon>}
+          onClick={newButtonOnClick}>
+            {newButtonText}
+        </Button>
+      }
 
-      <Divider variant="middle" orientation="vertical" />
+      {backButtonVisible && 
+        <Divider variant="middle" orientation="vertical" />
+      }
 
-      <Button 
-        variant="outlined" 
-        color="primary" 
-        disableElevation
-        startIcon={<Icon>arrow_back</Icon>}
-        onClick={undefined}
-      >
-        Voltar
-      </Button>
+      {backButtonVisible &&
+        <Button 
+          variant="outlined" 
+          color="primary" 
+          disableElevation
+          startIcon={<Icon>arrow_back</Icon>}
+          onClick={backButtonOnClick}>
+            Voltar
+        </Button>
+      }
 
     </Box>
 
